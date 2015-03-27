5f4065368bdebb8bb4292423cc4ae0d162f0a7d6
shub <shu8@users.noreply.github.com>
2015-03-27 16:26:18 +0000

    Update and rename linkedRelatedQuestionsAnswerCount.js to linkedRelatedQuestionsAnswerCount.user.js

--- Stand-alone-scripts/linkedRelatedQuestionsAnswerCount.user.js
+++ Stand-alone-scripts/linkedRelatedQuestionsAnswerCount.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Linked questions answer count
+// @name         Show answer count for questions in the sidebar
 // @namespace    http://stackexchange.com/users/4337810/%E1%94%95%E1%96%BA%E1%98%8E%E1%95%8A
 // @version      1.0
-// @description  Allows you to hover over questions in the sidebar to get their answer count
+// @description  Shows the answer count for related or linked questions as a tooltip (in the sidebar)
 // @author       ᔕᖺᘎᕊ (http://stackexchange.com/users/4337810/%E1%94%95%E1%96%BA%E1%98%8E%E1%95%8A)
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*
@@ -13,23 +13,15 @@
 // @match        *://*.mathoverflow.net/*
 // @grant        none
 // ==/UserScript==
-
 $('.sidebar-linked .linked .spacer a, .sidebar-related .related .spacer a').each(function(i) {
-    var id = $(this).attr('href').split('/')[2],
-      //id = $(this).attr('href').split('/')[4].split('?')[0]  
-        sitename = $(location).attr('hostname').split('.')[0],
-        that = $(this);    
-    
-    if (!isNaN(id)) {
-        id = $(this).attr('href').split('/')[4].split('?')[0];
-    }
-    
-    if (i % 2 == 0) { //even only (ie. only for the vote count square box)
+    if (!i % 2 == 0) { //odd only (ie. question title)
+        var id = $(this).attr('href').split('/')[2],
+            sitename = $(location).attr('hostname').split('.')[0],
+            that = $(this);
+
         $.getJSON("https://api.stackexchange.com/2.2/questions/" + id + "?order=desc&sort=activity&site=" + sitename, function(json) {
             answers = json.items[0].answer_count;
-            text = ' answers';
-            if (answers == 1) { text = ' answer'; }
-            that.attr('title', answers + text);
+            that.attr('title', answers + (answers == 1 ? ' answer' : ' answers'));
         });
     }
 });
