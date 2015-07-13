7cd356b545e515dd4665efa34b2d83f2e5714840
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2015-07-13 15:38:10 +0100

    Update features.user.js

--- features.user.js
+++ features.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SE Additional Optional Features
 // @namespace    http://stackexchange.com/users/4337810/
-// @version      1.5 DEV
+// @version      1.5
 // @description  Adds a bunch of optional features to the StackExchange sites.
 // @author       ᔕᖺᘎᕊ (http://stackexchange.com/users/4337810/)
 // @match        *://*.stackexchange.com/*
