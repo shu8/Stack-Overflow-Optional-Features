a335e11c5ecd3af5aa6dad3fdb2d8b5fadc26c9d
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2015-07-18 09:58:31 +0100

    Update features.user.js

--- features.user.js
+++ features.user.js
@@ -881,7 +881,7 @@ var functionsToCall = { //ALL the functions must go in here
         var apiLink = "https://api.stackexchange.com/2.2/questions?pagesize=5&order=desc&sort=activity&site=" + metaName;
 
         $('.topbar-links').prepend('<span id="mod-extra-icon" class="reputation links-container diamondOff">♦</span>');
-        $('.js-topbar-dialog-corral').prepend('<div class="topbar-dialog help-dialog dno" id="newMetaQuestionsDialog" style="top: 34px; left: 380px; display: none; z-index:1001;">\
+        $('.js-topbar-dialog-corral').prepend('<div class="topbar-dialog help-dialog dno" id="newMetaQuestionsDialog" style="top: 34px; left: 380px; display: none;">\
 <div class="modal-content" id="newMetaQuestionsList" style="max-height:none"><span id="closeNewQuestionList" style="float:right">x</span>\
 <ul>\
 <li>\
