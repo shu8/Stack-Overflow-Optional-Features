58d1bb4be352c92ca5923eda5a8d34f8d33ca36e
ᔕᖺᘎᕊ <shu8@users.noreply.github.com>
2015-08-17 17:55:19 +0100

    ;

--- features.user.js
+++ features.user.js
@@ -211,7 +211,7 @@ var functionsToCall = { //ALL the functions must go in here
         }
         
         $("#rep-card-next .percent").after($("#rep-card-next .label").css("z-index",0)).css("position","absolute");
-        $("#badge-card-next .percent").after($("#badge-card-next .label").css("z-index",0)).css("position","absolute")
+        $("#badge-card-next .percent").after($("#badge-card-next .label").css("z-index",0)).css("position","absolute");
     },
 
     highlightQuestions: function() { // For highlighting only the tags of favorite questions
